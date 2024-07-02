e43bacef46b3fbd7bd46489eedbe80f718d96c83
João Barbosa <87210017+JoaoSBarbosa@users.noreply.github.com>
2024-07-01 22:43:06 -0300

    Update SubscriberIssuingCard.java

--- mscards/src/main/java/com/joaobarbosa/mscards/infra/mqueue/SubscriberIssuingCard.java
+++ mscards/src/main/java/com/joaobarbosa/mscards/infra/mqueue/SubscriberIssuingCard.java
@@ -1,5 +1,4 @@
 package com.joaobarbosa.mscards.infra.mqueue;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joaobarbosa.mscards.application.services.exceptions.ControllerNotFoundException;
