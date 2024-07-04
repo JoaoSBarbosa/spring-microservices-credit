4753ab9c20ebc073da366852de9dc57ab372056f
João ubuntu <contato.jsbarbosa@gmail.com>
2024-07-04 20:50:04 -0300

    Aplicando proteção do Eureka com Spring security

---
+++ eurekaServer/src/main/java/com/joaobarbosa/microservice/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.joaobarbosa.microservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated() // autorizando todas as requisições que estiverem autenticadas
+                .and()
+                .httpBasic();
+    }
+}
