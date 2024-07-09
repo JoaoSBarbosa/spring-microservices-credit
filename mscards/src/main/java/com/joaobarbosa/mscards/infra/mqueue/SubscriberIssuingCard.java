95475a5de388d1a8d994a52b0fc2996e2bd585ba
João Barbosa <contato.jsbarbosa@gmail.com>
2024-07-09 10:33:44 -0300

    Alteração no RabbitMq e Keycloak

--- mscards/src/main/java/com/joaobarbosa/mscards/infra/mqueue/SubscriberIssuingCard.java
+++ mscards/src/main/java/com/joaobarbosa/mscards/infra/mqueue/SubscriberIssuingCard.java
@@ -11,6 +11,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class SubscriberIssuingCard {
 
